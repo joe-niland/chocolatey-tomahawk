b2edac94f7f15ce8dfd189ef7cfd99c4a1f1e03d
Joe Niland <joe@originalmind.com.au>
2014-11-11 11:11:44 +1100

    updating icon url; fixing uninstall script to work on 32 bit systems

--- tomahawk/tools/chocolateyUninstall.ps1
+++ tomahawk/tools/chocolateyUninstall.ps1
@@ -5,12 +5,14 @@ try {
   $silentArgs = "/S"
   $validExitCodes = @(0)
 
+  $unfile = "${Env:ProgramFiles}\Tomahawk\uninstall.exe"
   $unfilex86 = "${Env:ProgramFiles(x86)}\Tomahawk\uninstall.exe"
-
-  if (Test-Path "$unfilex86") {$file = "$unfilex86"}
-
-  if (Test-Path "$unfilex86") {
-    Uninstall-ChocolateyPackage $packageName $fileType $silentArgs $file -validExitCodes $validExitCodes
+  
+  if (Test-Path "$unfile") { $file = "$unfile" }
+  if (Test-Path "$unfilex86") { $file = "$unfilex86" }
+  
+  if (Test-Path "$file") {
+      Uninstall-ChocolateyPackage $packageName $fileType $silentArgs $file -validExitCodes $validExitCodes
   }
 
   Write-ChocolateySuccess $packageName
